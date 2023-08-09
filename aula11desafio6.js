72f9764896d14c5669cec91aa9a58fa470c4f0d2
virtsilver <137646434+virtsilver@users.noreply.github.com>
2023-08-09 09:37:02 -0300

    Add files via upload

---
+++ aula11desafio6.js
@@ -0,0 +1,13 @@
+function verificarNumero() {
+  const numero = parseInt(prompt('Digite um número:'));
+  if(numero > 0 && numero <= 10 && numero % 2 === 0 && Number.isInteger(numero)){
+    return "correto: número é par, inteiro e está entre 1e 10.";
+    const numero = parseInt(prompt('numero correto')); 
+  } else {
+    return 'incorreto: o número não atende aos critérios.';
+    
+  }
+}
+
+const resultado = verificarNumero();
+console.log(resultado);
